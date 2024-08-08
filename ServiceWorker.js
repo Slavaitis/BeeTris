c12eb16a4976004e57eefe5de849b4d32c258b50
slavaitisfame <slavaitisfame@gmail.com>
2024-08-08 13:22:11 +0400

    v0.17

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
+      caches.open('v2').then(function(cache) {
         return cache.addAll([
           '/',
           '/Build/Beetris.data',
@@ -24,7 +24,7 @@ self.addEventListener('install', function(event) {
   });
   
   self.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['v1'];
+    var cacheWhitelist = ['v2'];
   
     event.waitUntil(
       caches.keys().then(function(keyList) {
