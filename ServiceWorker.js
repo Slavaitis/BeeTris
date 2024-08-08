f7ba73d61a7089de751c562289052bdf170a9574
slavaitisfame <slavaitisfame@gmail.com>
2024-08-08 11:58:21 +0400

    v0.13

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,22 +1,38 @@
-self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
-});
-
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: data.icon
-    });
-});
-
-self.addEventListener('sync', event => {
-    if (event.tag === 'mySync') {
-        event.waitUntil(syncData());
-    }
-});
-
-async function syncData() {
-    // Логика фоновой синхронизации
-    console.log('Синхронизация данных в фоне');
-}
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/Build/Beetris.data',
+          '/Build/Beetris.framework.js',
+          '/Build/Beetris.loader.js',
+          '/Build/Beetris.wasm',
+          '/Build/Beetris.worker.js',
+          '/index.html',
+          '/ServiceWorker.js',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['v1'];
+  
+    event.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
